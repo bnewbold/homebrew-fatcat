e10da9dd1ad80a17916e53b23efbd971d18025b4
Bryan Newbold <bnewbold@archive.org>
2021-02-16 16:09:10 -0800

    v0.1.1

--- HomebrewFormula/fatcat-cli.rb
+++ HomebrewFormula/fatcat-cli.rb
@@ -1,11 +1,11 @@
 class FatcatCli < Formula
-  version '0.1.0'
+  version '0.1.1'
   desc "Interface to Fatcat Catalog API."
   homepage "https://gitlab.com/bnewbold/fatcat-cli"
 
   if OS.mac?
       url "https://archive.org/download/ia-fatcat-cli-bin/fatcat-cli-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "70a187ccd8a76e970a88be6046adbea4fcbe68233bd8746da9cb38c74a2d0a2f"
+      sha256 "054514a7813cf73acde4e3109194ee9f31c40da9449956930cb1f5df5722a5e0"
   end
 
   def install
